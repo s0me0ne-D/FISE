37ac749db0fa678aaeefe21a3d95f23711217f1f
Denys <zdybeldenys@gmail.com>
2024-05-23 18:04:43 +0200

    change serach style

--- src/components/layout/Header/Search.jsx
+++ src/components/layout/Header/Search.jsx
@@ -1,17 +1,27 @@
-import { NavLink, useNavigate } from "react-router-dom";
-import { SearchImg } from "../../../images/icons/SearchImg";
-import { useEffect, useState } from "react";
-import { fetchSearch } from "../../../fetch/fetchSearch";
-import "./search.scss";
-import { URL } from "../../../store/URL_SORE";
-import haveNotPoster from "../../../images/haveNotPoster.png";
-import { CubeLoader } from "../../../images/CubeLoader";
+import { NavLink, useNavigate } from 'react-router-dom';
+import { SearchImg } from '../../../images/icons/SearchImg';
+import { useEffect, useState } from 'react';
+import { fetchSearch } from '../../../fetch/fetchSearch';
+import './search.scss';
+import { URL } from '../../../store/URL_SORE';
+import haveNotPoster from '../../../images/haveNotPoster.png';
+import { CubeLoader } from '../../../images/CubeLoader';
+import { useOutsideClick } from '../../../hooks/useOutsideClick';
 
 export const Search = () => {
 	const [searchVisible, setSearchVisible] = useState(false);
-	const [searchValue, setSearchValue] = useState("");
+	const [searchValue, setSearchValue] = useState('');
 	const [searchResults, setSearchResults] = useState([]);
 	const [showNoResults, setShowNoResults] = useState(false);
+
+	const closeSearch = () => {
+		setSearchVisible(false);
+		setSearchValue('');
+		setSearchResults([]);
+	};
+
+	const searchRef = useOutsideClick(() => closeSearch());
+
 	const navigation = useNavigate();
 	useEffect(() => {
 		setSearchResults([]);
@@ -35,10 +45,10 @@ export const Search = () => {
 	};
 	const handleKeyPress = (event) => {
 		if (searchResults) {
-			if (event.key === "Enter" && searchResults.length > 0) {
+			if (event.key === 'Enter' && searchResults.length > 0) {
 				navigation(`/search/${searchValue}`);
 				setSearchVisible(false);
-				setSearchValue("");
+				setSearchValue('');
 				setSearchResults([]);
 			} else {
 				return null;
@@ -46,46 +56,44 @@ export const Search = () => {
 		}
 	};
 
-	useEffect(() => {
-		document.querySelector("body").addEventListener("click", () => {
-			setSearchVisible(false);
-			setSearchValue("");
-			setSearchResults([]);
-		});
-	}, []);
 	return (
-		<div className="hesder-search-menu">
-			<div className="header-search-wrapper">
-				<SearchImg
-					onClick={(event) => {
-						setSearchVisible((prev) => !prev);
-						event.stopPropagation();
-					}}
-				/>
-				<input
-					type="search"
-					value={searchValue}
-					onClick={(event) => event.stopPropagation()}
-					onChange={changeSearchValue}
-					onKeyDown={handleKeyPress}
-					className={searchVisible ? "header-search active" : "header-search"}
-					placeholder="Search"
-				></input>
-			</div>
-			<div className="header-search-results">
+		<div ref={searchRef} className='hesder-search-menu'>
+			<button
+				onClick={(event) => {
+					setSearchVisible((prev) => !prev);
+					event.stopPropagation();
+				}}
+			>
+				<SearchImg />
+			</button>
+			{searchVisible && (
+				<div className='header-search-wrapper'>
+					<input
+						type='search'
+						value={searchValue}
+						onClick={(event) => event.stopPropagation()}
+						onChange={changeSearchValue}
+						onKeyDown={handleKeyPress}
+						className='search'
+						placeholder='Search'
+					></input>
+				</div>
+			)}
+			<div className='header-search-results'>
 				{searchResults.length > 0 ? (
-					<div className="search-results">
-						<div className="search-results-title">
+					<div className='search-results'>
+						<div className='search-results-title'>
 							<NavLink to={`/search/${searchValue}`}>All results</NavLink>
 						</div>
-						<ul className="rearch-results-main">
+						<ul className='rearch-results-main'>
 							{searchResults.map((movie, index) => {
 								if (index < 4) {
-									if (movie.media_type !== "person") {
+									if (movie.media_type !== 'person') {
 										return (
 											<NavLink
-												to={`/${movie.media_type === "movie" ? "movie" : "tv"}/id/${movie.id}`}
+												to={`/${movie.media_type === 'movie' ? 'movie' : 'tv'}/id/${movie.id}`}
 												key={index}
+												onClick={closeSearch}
 											>
 												<img
 													src={
@@ -93,15 +101,14 @@ export const Search = () => {
 															? URL.LAZY_LOAD_IMG_URL + movie.poster_path
 															: haveNotPoster
 													}
-													alt="poster"
-													className={!movie.poster_path ? "have-not" : null}
+													alt='poster'
+													className={!movie.poster_path ? 'have-not' : null}
 												/>
-												<div className="result-description">
-													<span className="result-title">
-														{" "}
+												<div className='result-description'>
+													<span className='result-title'>
 														{movie.name ? movie.name : movie.title}
 													</span>
-													<span className="result-date">
+													<span className='result-date'>
 														{movie.first_air_date ? movie.first_air_date : movie.release_date}
 													</span>
 												</div>
@@ -116,10 +123,10 @@ export const Search = () => {
 						</ul>
 					</div>
 				) : showNoResults ? (
-					<div className="no-results">No results found for "{searchValue}"</div>
+					<div className='no-results'>No results found for "{searchValue}"</div>
 				) : searchValue.length > 1 ? (
-					<div className="no-results loader">
-						<CubeLoader width={"30px"} height={"30px"} />
+					<div className='no-results loader'>
+						<CubeLoader width={'30px'} height={'30px'} />
 						<span>Loading ...</span>
 					</div>
 				) : null}
