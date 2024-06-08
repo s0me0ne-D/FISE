d35a46a1f990b2e7f8bc33a4b4d5bd2f7cffcf4c
Denys <zdybeldenys@gmail.com>
2024-06-08 19:19:16 +0200

    styled & added functions for user settings menu

--- src/components/layout/Header/Authentication/UserMenu/UserMenu.jsx
+++ src/components/layout/Header/Authentication/UserMenu/UserMenu.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import './userMenu.scss';
+import { NavLink } from 'react-router-dom';
+import { ProfileIcon } from '../../../../../images/icons/ProfileIcon';
+import { useAuth0 } from '@auth0/auth0-react';
 
-export const UserMenu = () => {
-	return <div className='user-menu'></div>;
+export const UserMenu = ({ isShow, onClick }) => {
+	const { logout } = useAuth0();
+
+	const handleLogout = () => {
+		logout({
+			logoutParams: {
+				returnTo: window.location.origin,
+			},
+		});
+	};
+
+	return (
+		<div className={`user-menu ${isShow ? 'active-user-menu' : ''}`}>
+			<NavLink to={'/'} className={'user-menu_option '} onClick={onClick}>
+				<ProfileIcon />
+				<span>Your profile</span>
+			</NavLink>
+			<span className='user-menu_line' />
+			<button className='user-menu_log-out' onClick={handleLogout}>
+				Log out
+			</button>
+		</div>
+	);
 };
--- src/components/layout/Header/Authentication/UserSettingsMenu.jsx
+++ src/components/layout/Header/Authentication/UserSettingsMenu.jsx
@@ -2,17 +2,22 @@ import { useAuth0 } from '@auth0/auth0-react';
 import React, { useState } from 'react';
 import './userSettingsMenu.scss';
 import { UserMenu } from './UserMenu/UserMenu';
+import { useOutsideClick } from '../../../../hooks/useOutsideClick';
 
 export const UserSettingsMenu = () => {
 	const { user } = useAuth0();
-	const [showUserMenu, setShowUserMenu] = useState(true);
+	const [showUserMenu, setShowUserMenu] = useState(false);
+
+	const userSettingsRef = useOutsideClick(() => setShowUserMenu(false));
+
+	const handleOnClick = () => setShowUserMenu(false);
 
 	return (
-		<div className='user-settings'>
-			<button className='user-settings_btn'>
+		<div ref={userSettingsRef} className='user-settings'>
+			<button className='user-settings_btn' onClick={() => setShowUserMenu((prev) => !prev)}>
 				<img src={user.picture} alt='user' />
 			</button>
-			{showUserMenu && <UserMenu />}
+			<UserMenu isShow={showUserMenu} onClick={handleOnClick} />
 		</div>
 	);
 };
