2a74f11ff52766307e6fa76214d365d7ee0d825e
Denys <zdybeldenys@gmail.com>
2024-06-15 18:49:01 +0200

    added icons into media menu

--- src/components/main/MediaPoster/AuthenticationMediaMenu/AuthenticationMediaMenu.jsx
+++ src/components/main/MediaPoster/AuthenticationMediaMenu/AuthenticationMediaMenu.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import './authenticationMediaMenu.scss';
+import { InfoIcon } from '../../../../assets/icons/InfoIcon';
+import { AddIcon } from '../../../../assets/icons/AddIcon';
 
 export const AuthenticationMediaMenu = () => {
-	return <div className='media-menu'></div>;
+	return (
+		<div className='media-menu'>
+			<span className='information'>
+				<InfoIcon />
+			</span>
+			<button className='add-media'>
+				<AddIcon />
+			</button>
+		</div>
+	);
 };
