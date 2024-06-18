d1a1f69dca7833a205868faa30cf64526ec8f02e
Denys <zdybeldenys@gmail.com>
2024-06-18 18:35:51 +0200

    search types

---
+++ src/fetch/fetchSearch.ts
@@ -0,0 +1,19 @@
+import { QueryParams } from '../interfaces/queryParams_interface';
+
+export const fetchSearch = ({ searchValue, pageNumber = 1 }: QueryParams) => {
+	const options = {
+		method: 'GET',
+		headers: {
+			accept: 'application/json',
+			Authorization: `${process.env.REACT_APP_AUTHORIZATION_TOKEN}`,
+		},
+	};
+
+	const searchResults = fetch(
+		`https://api.themoviedb.org/3/search/multi?query=${searchValue}&include_adult=false&language=en-US&page=${pageNumber}`,
+		options
+	)
+		.then((response) => response.json())
+		.catch((err) => console.error(err));
+	return searchResults;
+};
