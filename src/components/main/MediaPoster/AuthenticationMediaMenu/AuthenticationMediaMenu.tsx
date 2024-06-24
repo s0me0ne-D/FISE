ddf8195a72ba5642acfeca8ca336e14559545b87
Denys <zdybeldenys@gmail.com>
2024-06-24 16:42:46 +0200

    AuthenticationMediaMenu component types

--- src/components/main/MediaPoster/AuthenticationMediaMenu/AuthenticationMediaMenu.tsx
+++ src/components/main/MediaPoster/AuthenticationMediaMenu/AuthenticationMediaMenu.tsx
@@ -4,14 +4,18 @@ import { InfoIcon } from '../../../../assets/icons/InfoIcon';
 import { AddIcon } from '../../../../assets/icons/AddIcon';
 import { useDispatch } from 'react-redux';
 import { addFavorite } from '../../../../redux/favoritesMediaSlice';
+import { Media } from '../../../../interfaces/media_interface';
 
-export const AuthenticationMediaMenu = ({ media }) => {
+export const AuthenticationMediaMenu = ({ media }: { media: Media }) => {
 	const dispatch = useDispatch();
 	const handleOnClick = () => {
 		dispatch(addFavorite(media));
 	};
 	return (
-		<div className='media-menu' onClick={(event) => event.preventDefault()}>
+		<div
+			className='media-menu'
+			onClick={(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => event.preventDefault()}
+		>
 			<span className='information media-menu-option'>
 				<InfoIcon />
 			</span>
