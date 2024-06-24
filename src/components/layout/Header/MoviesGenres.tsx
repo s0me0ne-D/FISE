760e1d8bdd644f95e859dfea930f71f4f3e2a664
Denys <zdybeldenys@gmail.com>
2024-06-24 17:01:29 +0200

    MoviesGenres component types

--- src/components/layout/Header/MoviesGenres.tsx
+++ src/components/layout/Header/MoviesGenres.tsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom';
 
 export const MoviesGenres = () => {
 	return genresMovies.map((genre) => {
-		const genreId = genre.name.toLowerCase() + '=' + genre.id;
+		const genreUrl = genre.name.toLowerCase() + '=' + genre.id;
 		return (
-			<NavLink to={'movie/genre/' + genreId} className='genres-menu-genre' key={genre.id}>
+			<NavLink to={'movie/genre/' + genreUrl} className='genres-menu-genre' key={genre.id}>
 				{genre.name}
 			</NavLink>
 		);
