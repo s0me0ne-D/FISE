7a763daea3781b07a775a75bea5d2314c8075046
Denys <zdybeldenys@gmail.com>
2024-07-01 17:04:21 +0200

    fixed getByGenre query params

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -27,7 +27,7 @@ export const mediaApi = createApi({
 		getByGenre: builder.query<ApiResult<Media>, QueryParams>({
 			query: ({ mediaType, pageNumber = 1, genreId, sortBy, filterByReleaseYear }) =>
 				`discover/${mediaType}?include_adult=false&include_video=false&language=en-US&page=${pageNumber}&sort_by=${sortBy}${
-					filterByReleaseYear && `&${filterByReleaseYear}`
+					filterByReleaseYear ? `&${filterByReleaseYear}` : ''
 				}&with_genres=${genreId}`,
 		}),
 		getDetails: builder.query<MediaDetails, QueryParams>({
