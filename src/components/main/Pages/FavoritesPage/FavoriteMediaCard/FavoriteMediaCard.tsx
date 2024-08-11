679d613352fa6db59bd70999be6663e8272deed4
Denys <zdybeldenys@gmail.com>
2024-08-11 13:27:54 +0200

    changed zIndex on hover

--- src/components/main/Pages/FavoritesPage/FavoriteMediaCard/FavoriteMediaCard.tsx
+++ src/components/main/Pages/FavoritesPage/FavoriteMediaCard/FavoriteMediaCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './favoriteMediaCard.scss';
 import { Media, MediaType } from '../../../../../interfaces/media_interface';
 import { NavLink } from 'react-router-dom';
@@ -12,13 +12,21 @@ import { useGetDetailsQuery } from '../../../../../redux/api';
 import { MediaGenres } from '../../../../MediaGenres/MediaGenres';
 
 export const FavoriteMediaCard = ({ media }: { media: Media }) => {
+	const [zIndex, setZIndex] = useState<number>(9);
+
 	const mediaType: MediaType = media.title ? 'movie' : 'tv';
 
 	const trailerKeyUrl = useTrailerKeyUrl({ mediaType, id: media.id });
 
 	const { data: details } = useGetDetailsQuery({ mediaType, id: media.id });
 	return (
-		<NavLink to={`/${mediaType}/id/${media.id}`} className='favorite-media'>
+		<NavLink
+			to={`/${mediaType}/id/${media.id}`}
+			className='favorite-media'
+			style={{ zIndex: zIndex }}
+			onMouseEnter={() => setZIndex(999)}
+			onMouseLeave={() => setZIndex(9)}
+		>
 			<div className='favorite-media_container'>
 				<LazyLoadImage
 					src={URL.ORIGINAL_IMG_URL + media.backdrop_path}
